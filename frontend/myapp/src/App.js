021fb5587e4e7fe4eb807cb5f33f67ac534e8c9e
PranithKumarDudala <praneethprani7@gmail.com>
2024-06-20 10:00:57 +0530

    component2 added from component2 branch

--- frontend/myapp/src/App.js
+++ frontend/myapp/src/App.js
@@ -1,9 +1,10 @@
 import Welcome from './Components/Welcome';
+import Componet2 from './Components/Component2';
 import './App.css';
 
 function App() {
   return (
-    <Welcome /> 
+      <Componet2 />
   )
 }
 
---
+++ frontend/myapp/src/Components/Component2/index.js
@@ -0,0 +1,10 @@
+const Componet2 = () => {
+
+    return (
+        <div>
+            <h1>this is component2</h1>
+        </div>
+    )
+}
+
+export default Componet2

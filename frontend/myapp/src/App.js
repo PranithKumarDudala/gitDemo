3d9a948d368de7162ec55f6301be9565529918ee
PranithKumarDudala <praneethprani7@gmail.com>
2024-06-20 09:16:31 +0530

    frontend created on component1

---
+++ frontend/myapp/src/App.js
@@ -0,0 +1,10 @@
+import Welcome from './Components/Welcome';
+import './App.css';
+
+function App() {
+  return (
+    <Welcome /> 
+  )
+}
+
+export default App;
---
+++ frontend/myapp/src/Components/Welcome/index.js
@@ -0,0 +1,11 @@
+const Welcome = () => {
+
+    return (
+        <div>
+
+            <h1>This is welcome page</h1>
+        </div>
+    )
+}
+
+export default Welcome
